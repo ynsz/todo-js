541422b258dce86f9684f18fb70916ca28a1238d
ynsz <suzuki.yuna.0801@gmail.com>
2025-08-26 09:07:06 +0900

    タスクの追加機能（テキスト）

--- src/main.js
+++ src/main.js
@@ -1,1 +1,28 @@
 import './style.css';
+
+const onClickAdd = () => {
+    // テキストボックスの値を取得し、初期化する
+    const inputText = document.getElementById("add-text").value;
+    document.getElementById("add-text").value = "";
+
+    // li生成
+    const li = document.createElement("li");
+
+    // div生成
+    const div = document.createElement("div");
+    div.className = "list-row";
+
+    // p生成
+    const p = document.createElement("p");
+    p.className = "todo-item";
+    p.innerText = inputText;
+
+    // liタグの子要素に各要素を設定
+    div.appendChild(p);
+    li.appendChild(div);
+
+    // 未完了リストに追加
+    document.getElementById("incomplete-list").appendChild(li);
+}
+
+document.getElementById("add-button").addEventListener("click", onClickAdd);
