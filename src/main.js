4f0d7c5a1bf67d5c30a85acc66940c8bac9086b3
ynsz <suzuki.yuna.0801@gmail.com>
2025-08-26 09:39:47 +0900

    タスクの削除機能

--- src/main.js
+++ src/main.js
@@ -28,7 +28,9 @@ const onClickAdd = () => {
     const deleteButton = document.createElement("button");
     deleteButton.innerText = "削除";
     deleteButton.addEventListener("click", () => {
-        alert("削除");
+        // 押された削除ボタンの親にあるliタグを未完了リストから削除
+        const deleteTarget = deleteButton.closest("li");
+        document.getElementById("incomplete-list").removeChild(deleteTarget);
     });
 
     // liタグの子要素に各要素を設定
